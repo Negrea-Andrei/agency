0623705be45d092fd3dab1313caecfaf5403a71e
Negrea-Andrei <negrea97@yahoo.com>
2024-03-06 21:45:23 +0200

    start new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
-import Header from "./components/Header";
-
-const App = () => {
+export default function App() {
   return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-      </div>
-      <ButtonGradient />
-    </>
-  );
-};
-
-export default App;
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
