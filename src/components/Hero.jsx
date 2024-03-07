55bc184d31e1c2e0c322103292f831743f1ac60f
Negrea-Andrei <negrea97@yahoo.com>
2024-03-07 21:06:06 +0200

    modify the color of the text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = ({theme}) => {
                     
                 </div>
                 <div className='order-2 sm:order-1 space-y-5 sm:pr-32'>
-                    <p className='text-primary/100 text-2xl font-serif'>Effortless</p>
+                    <p className='text-[#1E90FF] text-2xl font-serif'>Effortless</p>
                     <h1 className='text-5xl font-semibold lg:text-7xl font-serif'>Rent a'Car</h1>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <button className='btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300'> Let's GO!</button>
